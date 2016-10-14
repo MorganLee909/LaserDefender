87cef1ba4ba2be11f9408e52d7df8c168cf097ee
MorganLee909 <MorganLee909@gmail.com>
2016-10-14 08:51:28 -0400

    First working copy

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -2,8 +2,19 @@
 using System.Collections;
 
 public class Laser : MonoBehaviour {
+    public bool playerLaser;
 
 	void OnTriggerEnter2D(Collider2D collider) {
-		Destroy(gameObject);
+
+        if (playerLaser) {
+            if (collider.tag == "Enemy" || collider.tag == "Shredder") {
+                Destroy(gameObject);
+            }
+        }
+        if (!playerLaser) {
+            if(collider.tag == "Player" || collider.tag == "Shredder") {
+                Destroy(gameObject);
+            }
+        }
 	}
 }
